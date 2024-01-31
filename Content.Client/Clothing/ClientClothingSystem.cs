6d2fe4e91f07bbe3d69382b02d5a755ab7d6360f
SentryPrimies <sentryprime92@gmail.com>
2024-01-31 20:59:03 +0300

    меня 21 раз ударили ножом. Сейчас я умру.Кхе...Кхе...💀💀

--- Content.Client/Clothing/ClientClothingSystem.cs
+++ Content.Client/Clothing/ClientClothingSystem.cs
@@ -42,7 +42,7 @@ public sealed class ClientClothingSystem : ClothingSystem
         {"id", "IDCARD"},
         {"pocket1", "POCKET1"},
         {"pocket2", "POCKET2"},
-        {"suitstorage", "BACKPACK"}, //Imperial edit
+        {"suitstorage", "SUITSTORAGE"},
     };
 
     [Dependency] private readonly IResourceCache _cache = default!;
