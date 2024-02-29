946b6f2f15f0e0e01015aae35c9c11c4655562c4
SentryPrimies <sentryprime92@gmail.com>
2024-02-29 19:54:59 +0300

    Глобальное наступление геймеров

---
+++ Content.Shared/Imperial/Modsuit/Zoom/ModsuitGetZoomEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Content.Shared.Imperial.Modsuit.Zoom;
+
+[ByRefEvent]
+public record struct ModsuitGetZoomEvent(Vector2 Zoom)
+{
+    public void Increase(float zoom)
+    {
+        if (Zoom.X >= zoom)
+            return;
+
+        Zoom = new Vector2(zoom, zoom);
+    }
+}
