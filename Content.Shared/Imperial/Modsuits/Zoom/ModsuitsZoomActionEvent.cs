472c048c409d8334fa73c571c13f9293b3403c43
SentryPrimies <sentryprime92@gmail.com>
2024-02-29 13:43:51 +0300

    Абдулла, я хочу

--- Content.Shared/Imperial/Modsuits/Zoom/ModsuitsZoomActionEvent.cs
+++ Content.Shared/Imperial/Modsuits/Zoom/ModsuitsZoomActionEvent.cs
@@ -2,6 +2,6 @@ using Content.Shared.Actions;
 
 namespace Content.Shared.Imperial.Modsuits.Zoom;
 
-public sealed partial class ModsuitZoomActionEvent : InstantActionEvent
+public sealed partial class ModsuitsZoomActionEvent : InstantActionEvent
 {
 }
