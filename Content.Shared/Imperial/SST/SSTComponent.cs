a13aa72b0543a0823ccec9b68ca2c515a812f84c
SentryPrimies <sentryprime92@gmail.com>
2024-03-04 15:32:09 +0300

    Выплаты каждому гражданину Монголии. Забрать.

--- Content.Shared/Imperial/SST/SSTComponent.cs
+++ Content.Shared/Imperial/SST/SSTComponent.cs
@@ -4,11 +4,11 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Set;
 
-namespace Content.Shared.Imperial.Modsuit;
+namespace Content.Shared.Imperial.SST;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-[Access(typeof(ModsuitSystem))]
-public sealed partial class ModsuitComponent : Component
+[Access(typeof(SSTSystem))]
+public sealed partial class SSTComponent : Component
 {
     [DataField, AutoNetworkedField]
     public List<EntProtoId> ActionIds = new();
--- Content.Shared/Imperial/SST/SSTSystem.cs
+++ Content.Shared/Imperial/SST/SSTSystem.cs
@@ -8,9 +8,9 @@ using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 using Content.Shared.Actions;
 
-namespace Content.Shared.Imperial.Modsuit;
+namespace Content.Shared.Imperial.SST;
 
-public sealed class ModsuitSystem : EntitySystem
+public sealed class SSTSystem : EntitySystem
 {
 
     [Dependency] private readonly SharedActionsSystem _action = default!;
@@ -19,21 +19,21 @@ public sealed class ModsuitSystem : EntitySystem
     {
         base.Initialize();
 
-        SubscribeLocalEvent<ModsuitComponent, MapInitEvent>(OnModsuitMapInit);
+        SubscribeLocalEvent<SSTComponent, MapInitEvent>(OnSSTMapInit);
     }
 
-    private void OnModsuitMapInit(Entity<ModsuitComponent> modsuit, ref MapInitEvent args)
+    private void OnSSTMapInit(Entity<SSTComponent> sst, ref MapInitEvent args)
     {
-        foreach (var actionId in modsuit.Comp.ActionIds)
+        foreach (var actionId in sst.Comp.ActionIds)
         {
-            if (!modsuit.Comp.Actions.ContainsKey(actionId) &&
-                _action.AddAction(modsuit, actionId) is { } newAction)
+            if (!sst.Comp.Actions.ContainsKey(actionId) &&
+                _action.AddAction(sst, actionId) is { } newAction)
             {
-                modsuit.Comp.Actions[actionId] = newAction;
+                sst.Comp.Actions[actionId] = newAction;
             }
         }
 
-        Dirty(modsuit);
+        Dirty(sst);
     }
 
 }
